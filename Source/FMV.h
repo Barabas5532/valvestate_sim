e8d25ffd945937b9cc1e703d26c6d249d74ab2e8
Barabas Raffai <barabas.raffai@gmail.com>
2019-06-17 11:26:59 +0100

    Added FMV filter class

--- Source/FMV.h
+++ Source/FMV.h
@@ -1,11 +1,22 @@
-/*
-  ==============================================================================
-
-    FMV.h
-    Created: 14 Jun 2019 9:45:03am
-    Author:  barabas
-
-  ==============================================================================
-*/
-
 #pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class FMVFilter
+{
+    public:
+    FMVFilter();
+    ~FMVFilter();
+
+    void prepare(dsp::ProcessSpec spec);
+    void process(dsp::AudioBlock<float> block);
+    void reset();
+
+    private:
+    dsp::IIR::Coefficients<float> design(float l, float m, float t);
+
+    dsp::IIR::Filter<float> filter;
+    float sampleRate;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(FMVFilter);
+}
