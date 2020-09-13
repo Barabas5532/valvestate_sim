d07b4fbbefd29e0275beac561259a2b1ab49be9f
Barabas Raffai <barabas.raffai@gmail.com>
2020-09-13 12:10:49 +0100

    Added GPL3 license

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,11 +1,20 @@
 /*
-  ==============================================================================
+    Copyright 2020 Barabas Raffai
 
-    This file was auto-generated!
+    This file is part of Valvestate Sim.
 
-    It contains the basic framework code for a JUCE plugin editor.
+    Valvestate Sim is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
 
-  ==============================================================================
+    Valvestate Sim is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with Valvestate Sim.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include "PluginProcessor.h"
