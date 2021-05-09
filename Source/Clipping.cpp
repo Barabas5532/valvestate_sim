c7aea241bc0d3bdd5c649196b8868aabe76140af
Barabas Raffai <barabas.raffai@gmail.com>
2021-05-09 13:24:09 +0100

    Use FIR oversampling, as the IIR seems unstable

--- Source/Clipping.cpp
+++ Source/Clipping.cpp
@@ -1,8 +1,9 @@
 #include "Clipping.h"
+#include "WaveShape.h"
 #define OVERSAMPLING_ORDER 3
 
 Clipping::Clipping() : oversampling(1, OVERSAMPLING_ORDER,
-        dsp::Oversampling<float>::filterHalfBandPolyphaseIIR)
+        dsp::Oversampling<float>::filterHalfBandFIREquiripple)
 {
     waveshaper.functionToUse = waveshape;
 }
@@ -11,18 +12,19 @@ Clipping::~Clipping() {}
 
 void Clipping::process(dsp::AudioBlock<float> block)
 {
-    // TODO fix oversampling. When added, audio cuts out after a split second.
-    //auto oversampledBlock = oversampling.processSamplesUp(block);
+    auto oversampledBlock = oversampling.processSamplesUp(block);
 
-    dsp::ProcessContextReplacing<float> ctx(block);
+    dsp::ProcessContextReplacing<float> ctx(oversampledBlock);
     waveshaper.process(ctx);
 
-    //oversampling.processSamplesDown(block);
+    oversampling.processSamplesDown(block);
 }
 
 void Clipping::prepare(dsp::ProcessSpec spec)
 {
     oversampling.initProcessing(spec.maximumBlockSize);
+
+    juce::Logger::outputDebugString("Oversampling latency (samples): " + std::to_string(oversampling.getLatencyInSamples()));
 }
 
 void Clipping::reset()
--- Source/Clipping.h
+++ Source/Clipping.h
@@ -10,7 +10,6 @@
 
 #pragma once
 #include "../JuceLibraryCode/JuceHeader.h"
-#include "WaveShape.h"
 
 class Clipping
 {
