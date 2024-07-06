4e9670f43915ed599348e336728bfc001cd32a23
Barabas Raffai <barabas.raffai@gmail.com>
2024-07-06 17:30:11 +0100

    Implement IIR3 external

--- pd/externals/iir3.c
+++ pd/externals/iir3.c
@@ -1,31 +1,120 @@
+#include "iir.h"
 #include "m_pd.h"
+#include <string.h>
 
-static t_class *helloworld_class;
+#define DSP_ARG_COUNT 4
+#define IIR_ORDER 3
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
-typedef struct _helloworld {
-  t_object  x_obj;
-} t_helloworld;
+static t_class *iir3_tilde_class;
 
+typedef struct iir3_tilde {
+  t_object self;
 
-void helloworld_bang(t_helloworld *x)
-{
-  post("Hello world !!");
+  float w[IIR_ORDER];
+  float coefficients[(IIR_ORDER + 1) * 2];
+
+  t_float b0;
+  t_float b1;
+  t_float b2;
+  t_float b3;
+
+  // a0 is normalised to 1
+  t_float a1;
+  t_float a2;
+  t_float a3;
+
+  t_inlet *in;
+
+  t_inlet *in_b0;
+  t_inlet *in_b1;
+  t_inlet *in_b2;
+  t_inlet *in_b3;
+
+  t_inlet *in_a1;
+  t_inlet *in_a2;
+  t_inlet *in_a3;
+
+  t_outlet *out;
+} t_iir3_tilde;
+
+t_int *iir3_tilde_perform(t_int *w) {
+  t_iir3_tilde *self = (t_iir3_tilde *)(w[1]);
+  t_sample *in = (t_sample *)(w[2]);
+  t_sample *out = (t_sample *)(w[3]);
+  int buffer_size = (int)(w[4]);
+
+  self->coefficients[0] = self->b0;
+  self->coefficients[1] = self->b1;
+  self->coefficients[2] = self->b2;
+  self->coefficients[3] = self->b3;
+  self->coefficients[4] = 1;
+  self->coefficients[5] = self->a1;
+  self->coefficients[6] = self->a2;
+  self->coefficients[7] = self->a3;
+
+  iir_process(in, out, buffer_size, self->coefficients, self->w,
+              ARRAY_SIZE(self->coefficients));
+
+  return w + DSP_ARG_COUNT + 1;
 }
 
-
-void *helloworld_new(void)
-{
-  t_helloworld *x = (t_helloworld *)pd_new(helloworld_class);
-  return (void *)x;
+void iir3_tilde_dsp(t_iir3_tilde *self, t_signal **sp) {
+  dsp_add(iir3_tilde_perform, DSP_ARG_COUNT, self, sp[0]->s_vec, sp[1]->s_vec,
+          sp[0]->s_n);
 }
 
-void helloworld_setup(void) {
-  helloworld_class = class_new(gensym("helloworld"),
-                               (t_newmethod)helloworld_new,
-                               0,
-                               sizeof(t_helloworld),
-                               CLASS_DEFAULT,
-                               0);
+void *iir3_tilde_new(t_symbol *s, int argc, t_atom *argv) {
+  t_iir3_tilde *self = (t_iir3_tilde *)pd_new(iir3_tilde_class);
 
-  class_addbang(helloworld_class, helloworld_bang);
+  self->b0 = 0;
+  self->b1 = 0;
+  self->b2 = 0;
+  self->b3 = 0;
+
+  self->a1 = 0;
+  self->a2 = 0;
+  self->a3 = 0;
+
+  memset(self->w, 0, sizeof self->w);
+  memset(self->coefficients, 0, sizeof self->w);
+
+  self->in = signalinlet_new(&self->self, 0.f);
+
+  self->in_b0 = floatinlet_new(&self->self, &self->b0);
+  self->in_b1 = floatinlet_new(&self->self, &self->b1);
+  self->in_b2 = floatinlet_new(&self->self, &self->b2);
+  self->in_b3 = floatinlet_new(&self->self, &self->b3);
+
+  self->in_a1 = floatinlet_new(&self->self, &self->a1);
+  self->in_a2 = floatinlet_new(&self->self, &self->a2);
+  self->in_a3 = floatinlet_new(&self->self, &self->a3);
+
+  self->out = outlet_new(&self->self, &s_signal);
+
+  return self;
+}
+
+void iir3_tilde_free(t_iir3_tilde *self) {
+  inlet_free(self->in);
+
+  inlet_free(self->in_b0);
+  inlet_free(self->in_b1);
+  inlet_free(self->in_b2);
+  inlet_free(self->in_b3);
+
+  inlet_free(self->in_a1);
+  inlet_free(self->in_a2);
+  inlet_free(self->in_a3);
+
+  outlet_free(self->out);
+}
+
+void iir3_tilde_setup(void) {
+  iir3_tilde_class = class_new(gensym("iir3~"), (t_newmethod)iir3_tilde_new,
+                               (t_method)iir3_tilde_free, sizeof(t_iir3_tilde),
+                               CLASS_DEFAULT, A_GIMME, 0);
+
+  class_addmethod(iir3_tilde_class, (t_method)iir3_tilde_dsp, gensym("dsp"),
+                  A_CANT, 0);
 }
