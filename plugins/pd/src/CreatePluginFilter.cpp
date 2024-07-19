5e36a8ff411b5aa42fb90428eea0684e4c63e27a
Barabas Raffai <barabas.raffai@gmail.com>
2024-07-19 22:53:34 +0100

    WIP integrate pd

---
+++ plugins/pd/src/CreatePluginFilter.cpp
@@ -0,0 +1,6 @@
+#include "PluginProcessor.h"
+#include "Valvestate.h"
+
+juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter() {
+  return new ValvestateAudioProcessor(JucePlugin_Name, std::make_unique<PureDataProcessor>());
+}
---
+++ plugins/pd/src/Processor.cpp
@@ -0,0 +1,5 @@
+#include "ValvestateProcessorBase.h"
+
+class PureDataProcessor final : public ValvestateProcessorBase {
+  
+};
