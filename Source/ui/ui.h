9225cd5eacb0c37f09d60ed896feb0a58751bbba
Barabas Raffai <barabas.raffai@gmail.com>
2025-01-06 13:10:21 +0000

    Do not draw toggle button in release mode

--- Source/ui/ui.h
+++ Source/ui/ui.h
@@ -109,46 +109,9 @@ public:
   }
 #endif
 
-#if 0
-    void drawToggleButton(Graphics &g, ToggleButton &b, 
-            bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown)
-    {
-        (void) shouldDrawButtonAsHighlighted;
-        (void) shouldDrawButtonAsDown;
-
-        auto width = b.getWidth();
-        auto height = b.getHeight();
-        float margin = 15;
-        auto radius = jmin (width / 2, height / 2) - margin;
-
-        Image switchImage[5];
-        switchImage[0] = ImageCache::getFromMemory (BinaryData::switchup_png,
-                (size_t)BinaryData::switchup_png);
-        switchImage[1] = ImageCache::getFromMemory (BinaryData::switchcentre_png,
-                (size_t)BinaryData::switchcentre_pngSize);
-        switchImage[2] = ImageCache::getFromMemory (BinaryData::switchdown_png,
-                (size_t)BinaryData::switchdown_pngSize);
-        int index;
-
-        auto centreX = b.getWidth()/2;
-        auto centreY = b.getHeight()/2;
-
-        if(b.getToggleState())
-        {
-            //draw up switch
-            index = 0;
-        }
-        else
-        {
-            //draw down switch
-            index = 2;
-        }
-
-        AffineTransform transform;
-        transform = transform.scaled(2*radius/switchImage[0].getWidth(), 2*radius/switchImage[0].getHeight());
-        transform = transform.translated(margin, (height/2-radius));
-        g.drawImageTransformed(switchImage[index], transform);
-    }
+// Do not draw toggle button in release mode
+#if !_DEBUG
+  void drawToggleButton(Graphics &, ToggleButton &, bool, bool) override {}
 #endif
 };
 
