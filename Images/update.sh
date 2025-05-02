d62295256246e0183f4793526974a55961857060
Barabas Raffai <barabas.raffai@gmail.com>
2025-05-02 10:33:57 +0100

    clean up

--- Images/update.sh
+++ Images/update.sh
@@ -18,21 +18,3 @@ do
   echo "$knob"
   magick montage -mode concatenate -background transparent -tile 1x $1/$knob/* Images/$knob.png
 done
-
-# Copy images from render output to project, and fix alpha channel data
-
-# left pad with zeros to 4 characters
-#string(LENGTH "${knob_index}" length)
-#set(padded_knob_index "${knob_index}")
-#while(length LESS 4)
-#    set(padded_knob_index "0${padded_knob_index}")
-#    math(EXPR length "${length} + 1")
-#endwhile()
-#
-#set(knob_file "${knob_name}/Image${padded_knob_index}.png")
-#set(out_file "${CMAKE_CURRENT_BINARY_DIR}/${knob_name}/Image${padded_knob_index}.png")
-#add_custom_command(OUTPUT ${out_file}
-#                   COMMAND ${IMAGEMAGICK_CONVERT} ${CMAKE_CURRENT_LIST_DIR}/${knob_file} -channel A -gamma 0.45 +channel ${out_file}
-#                   MAIN_DEPENDENCY ${knob_file}
-#)
-
